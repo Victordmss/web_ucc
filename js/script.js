84660c2ddcd15004b3ab91f9aea9892ca80dbee6
VictorD <victor.demessance@gmail.com>
2024-11-06 16:04:17 +0100

    [*] Refactor css & js script headers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+/* ================================================================================ */
+/*                          SCRIPT DU SITE WEB DE L'ASSOCIATION UCC                   */
+/* ================================================================================ */
+/* Nom du fichier     : script.js                                                   */
+/* Type de fichier    : fichier JavaScript (.js)                                    */
+/* Créateur           : Victor Demessance                                          */
+/* Créé le            : 08/10/2024                                                 */
+/* ================================================================================ */
+
+
+
+// Fonction d'affichage du texte supplémentaire de l'historique de l'association
 function toggleText() {
     var text = document.getElementById("additional-text");
     if (text.style.display === "none") {
