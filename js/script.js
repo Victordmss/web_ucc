5652d239acb94a62350f0b2d2c455fb992bc5434
VictorD <victor.demessance@gmail.com>
2024-11-06 15:59:49 +0100

    [*] Refactor actu/contact/footer sections

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-fetch('/includes/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    })
-    .catch(error => console.log('Erreur de chargement du header:', error));
-
 function toggleText() {
     var text = document.getElementById("additional-text");
     if (text.style.display === "none") {
